ec28e6ef7ae212c81cc18fc6ad9bbed11dfce0f8
hh <zhang.hilary28@gmail.com>
2020-08-14 00:31:48 +0800

    building environment

---
+++ demo1.js
@@ -0,0 +1,5 @@
+// ts文件不能直接允许
+// ts file
+var a = 123;
+var str = '123';
+console.log(a);
---
+++ demo1.ts
@@ -0,0 +1,7 @@
+// ts文件不能直接允许
+// ts file
+// ts-node 直接运行ts文件 npm install ts-node -D
+// npx ts-node file.ts
+let a: number = 123;
+
+console.log(a)
